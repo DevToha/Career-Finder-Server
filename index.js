6c5590f9a63b59d8087530068b000212ed960073
Deptoha <tohakhan249156@gmail.com>
2024-05-14 16:29:56 +0600

    added post method for applied job

--- index.js
+++ index.js
@@ -65,6 +65,57 @@ async function run() {
             res.send(result)
         })
 
+        // Update job 
+
+        app.put('/job/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true };
+            const updatedJob = req.body
+
+            const job = {
+                $set: {
+                    job_title: updatedJob.job_title,
+                    job_category: updatedJob.job_category,
+                    salary_range: updatedJob.salary_range,
+                    job_description: updatedJob.job_description,
+                    user_name: updatedJob.user_name,
+                    job_posting_date: updatedJob.job_posting_date,
+                    application_deadline: updatedJob.application_deadline,
+                    job_applicants_number: updatedJob.job_applicants_number,
+                    user_email: updatedJob.user_email,
+                    job_banner_url: updatedJob.job_banner_url
+                }
+            }
+
+            const result = await jobCollection.updateOne(filter, job, options);
+            res.send(result)
+
+        })
+
+        // applied job
+        const appliedJobCollection = client.db('JobDB').collection('appliedJob');
+
+        app.post('/appliedJob', async (req, res) => {
+            const newAppliedJob = req.body
+            console.log(newAppliedJob)
+            const result = await appliedJobCollection.insertOne(newAppliedJob)
+            res.send(result)
+        })
+
+        // get applied jon in applied page
+
+        app.get('/appliedJob', async (req, res) => {
+            const cursor = appliedJobCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        app.get("/appliedJob/:LoginEmail", async (req, res) => {
+            console.log(req.params.LoginEmail)
+            const result = await appliedJobCollection.find({ LoginEmail: req.params.LoginEmail }).toArray();
+            res.send(result)
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
