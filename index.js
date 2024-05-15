1cd77b33b9a8c7100a49e0c86e98a52945bf78ca
Deptoha <tohakhan249156@gmail.com>
2024-05-15 22:09:10 +0600

    added some change for vercel deploy

--- index.js
+++ index.js
@@ -9,7 +9,15 @@ const port = process.env.PORT || 5000
 
 // middleware
 
-app.use(cors())
+app.use(
+    cors({
+        origin: ["http://localhost:5173",
+            "https://assignment-11-f5d5e.web.app",
+            "https://assignment-11-f5d5e.firebaseapp.com"],
+        credentials: true,
+    })
+);
+
 app.use(express.json())
 
 
@@ -28,7 +36,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const jobCollection = client.db('JobDB').collection('job');
 
@@ -134,7 +142,7 @@ async function run() {
         })
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
